25055f92c416a7de226204328b38934b1a2c3c9d
kenboo98 <tellambu@ualberta.ca>
2020-05-08 20:32:00 -0600

    Made the querying async

--- src/hn_api.rs
+++ src/hn_api.rs
@@ -1,8 +1,11 @@
 use std::collections::HashMap;
 use reqwest::blocking;
+use reqwest::Client;
 use std::io::{Error, ErrorKind};
 use serde_json::{Value, Number, Map};
-use std::borrow::BorrowMut;
+
+use futures::future::join_all;
+use tokio::runtime::Runtime;
 
 const URI_PREFIX: &str = "https://hacker-news.firebaseio.com/v0/";
 
@@ -15,15 +18,15 @@ const URI_JOB_STORIES: &str = "jobstories";
 
 const URI_ITEM: &str = "item/";
 
-pub enum StoryType{
+pub enum StoryType {
     TopStories,
     NewStories,
     BestStories,
     AskStories,
     ShowStories,
-    JobStories
-
+    JobStories,
 }
+
 pub fn get_stories(story_type: &StoryType) -> Result<Vec<String>, Error> {
     let endpoint = match story_type {
         StoryType::TopStories => URI_TOP_STORIES,
@@ -35,25 +38,32 @@ pub fn get_stories(story_type: &StoryType) -> Result<Vec<String>, Error> {
     };
     let resp = match blocking::get(format!("{}{}.json", URI_PREFIX, endpoint).as_str()) {
         Ok(resp) => resp,
-        Err(e) => return Err(Error::new(ErrorKind::NotConnected, "Could not access HackerNews"))
+        Err(_) => return Err(Error::new(ErrorKind::NotConnected, "Could not access HackerNews"))
     };
     let content: Vec<Number> = resp.json().expect("Could not get response text.");
     let items = content.into_iter().map(|val: Number| val.to_string()).collect();
     return Ok(items);
-
 }
 
 pub fn get_items(ids: &[String]) -> Result<Vec<Map<String, Value>>, Error> {
-    let client = blocking::Client::new();
-    let mut result: Vec<Map<String, Value>> = vec!();
-    for id in ids {
-        let resp = match client.get(format!("{}{}{}.json", URI_PREFIX, URI_ITEM, id.as_str()).as_str()).send() {
-            Ok(resp) => resp,
-            Err(e) => return Err(Error::new(ErrorKind::NotConnected, "Could not access HackerNews"))
-        };
-        let content: Map<String, Value> = resp.json().expect("Could not get response text.");
-        result.push(content);
-    }
-    Ok(result)
+    let mut rt = Runtime::new()?;
+    let client = Client::new();
+    let results = rt.block_on(
+        join_all(
+            ids
+                .iter()
+                .map(|id| {
+                    client.get(format!("{}{}{}.json", URI_PREFIX, URI_ITEM, id.as_str()).as_str()).send()
+                })
+        )
+    );
 
+
+    let items: Vec<Map<String, Value>> = results
+        .into_iter()
+        .map(|result| {
+            let resp = result.expect("Could not get response");
+            rt.block_on(resp.json()).expect("Could not get response text.")
+        }).collect();
+    Ok(items)
 }
